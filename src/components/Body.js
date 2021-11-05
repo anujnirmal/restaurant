b8c2264701e3fa61594fd083332d622f8528e7d2
Anuj Nirmal <anujrajeshnirmal@gmail.com>
2021-11-06 00:41:53 +0530

    Added the backdrop page and some other components

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,10 +3,18 @@ import image from "../assets/image.jpg";
 
 function Body() {
     return (
-        <div>
+        <div className="body-container">
             <img className="backdrop" src={image} alt="back-image" />
-            <div>
-                <h1 className="title-body">Welcome to Cafe Restaurant</h1>
+            <div className="container body-main">
+                <h1 className="title-body">Welcome to <br /><span className="body-text">Cafe Restaurant</span></h1>
+                <p className="para-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt erat <br />pretium bibendum dictum. In sapien nulla, tempor ut dapibus aliquam, sodales<br /> sit amet nisi. Mauris massa felis,</p>
+                <button className="button-body">See Menu</button>
+            </div>
+            <div className="card">
+                <h4>Our Top Picks</h4>
+            </div>
+            <div className="card-back">
+
             </div>
         </div>
     );
